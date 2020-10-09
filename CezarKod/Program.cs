a17642b6289e25b5ae46c233000735dacb92511a
Kajetan Jedrych <jedrychk@gmail.com>
2020-10-09 10:53:04 +0200

    First commit saving text to tabel

---
+++ CezarKod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CezarKod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != null && input != "")
+            {
+                //Console.WriteLine(input);
+
+                char[] arr;
+                arr = input.ToCharArray(0, input.Length);
+                foreach (char item in arr)
+                   Console.Write(item);
+            }
+            //for (int i = 0; i <= input.Length; i++)
+            //{
+               // ConsoleKeyInfo keyInfo;
+               // do
+               //   keyInfo = Console.ReadKey(true);
+                 //   if (keyInfo.KeyChar >= 65 && keyInfo.KeyChar <= 90)
+                  //  {
+               //         char znak = (char)(((keyInfo.KeyChar - 62) % 28) + 65);
+                 //       Console.WriteLine(znak);
+                //    }
+                //} while (keyInfo.Key != ConsoleKey.Escape);
+           //}
+        }
+    }
+}
