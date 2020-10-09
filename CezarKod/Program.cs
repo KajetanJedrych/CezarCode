d6ea80b27304fb0dc8eed7370d542d87d39c1a73
Kajetan Jedrych <jedrychk@gmail.com>
2020-10-09 11:11:31 +0200

    some test of code

--- CezarKod/Program.cs
+++ CezarKod/Program.cs
@@ -9,25 +9,26 @@ namespace CezarKod
             string input;
             while ((input = Console.ReadLine()) != null && input != "")
             {
-                //Console.WriteLine(input);
+                
 
                 char[] arr;
                 arr = input.ToCharArray(0, input.Length);
-                foreach (char item in arr)
-                   Console.Write(item);
+
+                foreach (char item in arr) ;
+                 // Console.Write(item);
             }
-            //for (int i = 0; i <= input.Length; i++)
-            //{
-               // ConsoleKeyInfo keyInfo;
-               // do
-               //   keyInfo = Console.ReadKey(true);
-                 //   if (keyInfo.KeyChar >= 65 && keyInfo.KeyChar <= 90)
-                  //  {
-               //         char znak = (char)(((keyInfo.KeyChar - 62) % 28) + 65);
-                 //       Console.WriteLine(znak);
-                //    }
-                //} while (keyInfo.Key != ConsoleKey.Escape);
-           //}
+            for (int i = 0; i <= input.Length; i++)
+            {
+                ConsoleKeyInfo keyInfo;
+                do
+                  keyInfo = input ;
+                    if (keyInfo.KeyChar >= 65 && keyInfo.KeyChar <= 90)
+                    {
+                        char znak = (char)(((keyInfo.KeyChar - 62) % 28) + 65);
+                       Console.WriteLine(znak);
+                    }
+                    while (keyInfo.Key != ConsoleKey.Escape);
+            }
         }
     }
 }
