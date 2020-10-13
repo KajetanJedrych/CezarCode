59d4b75393939da14084fadced2c8d691e9d7527
Kajetan Jedrych <jedrychk@gmail.com>
2020-10-13 11:31:54 +0200

    new type of convert

--- CezarKod/Program.cs
+++ CezarKod/Program.cs
@@ -14,21 +14,29 @@ namespace CezarKod
                 char[] arr;
                 arr = input.ToCharArray(0, input.Length);
 
-                foreach (char item in arr) ;
-                 // Console.Write(item);
+                foreach (var item in arr) 
+                {
+                    Console.Write(Convert.ToChar((item - 'A' + 3) % ('X' - 'A' + 3) + 'A'));
+                } 
+                
             }
-            for (int i = 0; i <= input.Length; i++)
-            {
-                ConsoleKeyInfo keyInfo;
-                do
-                  keyInfo =  ;
-                    if (keyInfo.KeyChar >= 65 && keyInfo.KeyChar <= 90)
-                    {
-                        char znak = (char)(((keyInfo.KeyChar - 62) % 28) + 65);
-                       Console.WriteLine(znak);
-                    }
-                    while (keyInfo.Key != ConsoleKey.Escape);
-            }
+
+
+
+
+
+           // for (int i = 0; i <= input.Length; i++)
+           // {
+               // ConsoleKeyInfo keyInfo;
+               // do
+                 // keyInfo =  ;
+                  //  if (keyInfo.KeyChar >= 65 && keyInfo.KeyChar <= 90)
+                    //{
+                     //   char znak = (char)(((keyInfo.KeyChar - 62) % 28) + 65);
+                     //  Console.WriteLine(znak);
+                   // }
+                  //  while (keyInfo.Key != ConsoleKey.Escape);
+           // }
         }
     }
 }
